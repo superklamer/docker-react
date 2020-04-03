d209e2a518ef46bd660714a4c3934a49090ce23a
Radoslav Radoev <rradoev@gmail.com>
2020-04-03 00:13:47 -0700

    initial commit

---
+++ build/precache-manifest.03a94862bbc3c98a2ac9a3f3ee6ef9fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08447d8ccfc37526797b05741e5bd16f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89c3a13f58d51010a096",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b5354aeb1deebc45ba92",
+    "url": "/static/js/2.9b019f0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b019f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c3a13f58d51010a096",
+    "url": "/static/js/main.8ff0b83f.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
